c217aff5d11f9b6d2c94e7b9b237431961a7b2b8
JuanMBriones <JuanMBriones@outlook.com>
2021-06-30 21:37:10 -0500

    Added Dynamic Experiment support

--- modules/sender.py
+++ modules/sender.py
@@ -8,7 +8,7 @@ class Sender:
     connector = None
 
     headers = {}
-    record = {'file': '', 'hardware': "1"}
+    record = {'file': '', 'hardware': "1", 'experiment': ""}
 
 
     def __init__(self):
@@ -19,12 +19,47 @@ class Sender:
         else:
             print('Error: Null Connector')
 
+
+    def get_last_experiment(self):
+        if self.connector.active_conn() :
+            url = urlsData['baseApi'] + urlsData['lastexperiments'] + '/'
+
+            response = requests.get(url, headers=self.headers).json()
+            try:
+                experiment_id = int(response[0]['experiment'])
+
+                return experiment_id
+            except:
+                return 0
+
+
+            return experiment_id
+
+    def create_experiment(self):
+        experiment = {} #{'favorite': '', 'description': ''}
+
+        if self.connector.active_conn() :
+            url = urlsData['baseApi'] + urlsData['experiments'] + '/'
+            experiment['favorite'] = False
+            experiment['description'] = 'A new experiment'
+
+            response = requests.post(url, data=json.dumps(experiment), headers=self.headers).json()
+            return response
+
     def post_record(self, file, type):
         if self.connector.active_conn() :
             url = urlsData['baseApi'] + urlsData['records'] + '/'
             self.record['hardware'] = type
             self.record['file'] = file
 
+            last_experiment_id = self.get_last_experiment()
+            if last_experiment_id:
+                self.record['experiment'] = str(last_experiment_id)
+            else:
+                newExperimentID = self.create_experiment()['experimentData']['id']
+                self.record['experiment'] = str(newExperimentID)
+            #print(str(self.record['experiment']))
+            #print(json.dumps(self.record))
             response = requests.post(url, data=json.dumps(self.record), headers=self.headers).json()
             print(response)
 
